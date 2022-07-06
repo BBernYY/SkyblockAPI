b660fa45ccb6dce65fbdbd10bef47c221f6aa196
Sytze Reitsma <66414852+BBernYY@users.noreply.github.com>
2022-07-06 12:16:22 +0200

    Update kwargs.py

--- kwargs.py
+++ kwargs.py
@@ -1,6 +1,6 @@
 kwargs = {
-    "_": True, # toggle use of this file
-    "hypixel_api_key": "4a8f48a5-47a5-43f1-81d8-5d1e71efeebe",
-    "webhook_link": "https://discord.com/api/webhooks/993789862006235167/ftV2GxCmjh9vlDKkgN8USG5Os_AhvAeV_5Nz1oeW7GYxOI_OlYF14Qsdg5oHRnCmREON",
+    "_": False, # toggle use of this file 
+    "hypixel_api_key": "",
+    "webhook_link": "",
     "name": "BBernYY"
 }
