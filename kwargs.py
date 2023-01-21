1a9b11129265e204892223ce09699b893564ac6c
SytzeReitsma <>
2023-01-21 19:11:44 +0100

    update

--- kwargs.py
+++ kwargs.py
@@ -1,6 +1,9 @@
 kwargs = {
-    "_": False, # toggle use of this file
-    "hypixel_api_key": "4a8f48a5-47a5-43f1-81d8-5d1e71efeebe",
-    "webhook_link": "https://discord.com/api/webhooks/993789862006235167/ftV2GxCmjh9vlDKkgN8USG5Os_AhvAeV_5Nz1oeW7GYxOI_OlYF14Qsdg5oHRnCmREON",
+    "_": True, # toggle use of this file
+    "hypixel_api_key": "8a64a9de-8d79-4527-847c-341b5be899e5",
+    "webhook_link": "https://discord.com/api/webhooks/1066359365659009106/P_hpxQ14TwYdP81qsKa0pY16RH4Nirl1aDIMrsGl3-iLabva8EEi1tEbQlkEdqtd7AgR",
     "name": "BBernYY"
 }
+if __name__ == '__main__':
+    from main import main
+    main()
--- main.py
+++ main.py
@@ -1,30 +1,61 @@
-def send_message(webhook_link, content): # function for uploading data 
+def send_message(webhook_link, embed): # function for uploading data 
     from discord_webhook import DiscordWebhook
-    webhook = DiscordWebhook(url=webhook_link, content=content)
+    webhook = DiscordWebhook(url=webhook_link)
+    webhook.add_embed(embed)
     webhook.execute() # send message on webhook
-def write_message(session, name): # function for formatting data
-    import datetime as dt
-    now = dt.datetime.now(dt.timezone(dt.timedelta(hours=+2))).strftime('`%B %d %Y` at `%H:%M:%S`')
-    if session['online']:
-        message = f"`{name}` has joined `{session['mode']}` in `{session['gameType']}` on {now}"
+
+def generate_embed(session, name, now, old_session, old_now):
+    from discord_webhook import DiscordEmbed
+    from datetime import datetime as dt
+    if session != "None":
+        embed = DiscordEmbed(title="Update!", description=f'`{name}` has joined `{session["gameType"]}` in `{session["mode"]}` on {now}')
+        embed.add_embed_field(name='Current game', value=f"`{session['gameType']}`")
+        embed.add_embed_field(name='Current mode', value=f"`{session['mode']}`")
+        embed.add_embed_field(name='Current time', value=now)
+        if old_session['online']:
+            embed.add_embed_field(name='Previous game', value=f"`{old_session['gameType']}`")
+            embed.add_embed_field(name='Previous mode', value=f"`{old_session['mode']}`")
+            time_spent = dt.strptime(now, '`%B %d %Y`, `%H:%M:%S`') - dt.strptime(old_now, '`%B %d %Y`, `%H:%M:%S`')
+            time_spent = time_spent.seconds
+            hours = (time_spent-time_spent%3600) // 3600
+            time_spent -= hours * 3600
+            mins = (time_spent-time_spent%60) // 60
+            time_spent -= mins * 60
+            embed.add_embed_field(name='Time spent', value=f'`{str(hours).zfill(2)}:{str(mins).zfill(2)}:{str(time_spent).zfill(2)}`')
+        embed.set_color('0x00ff00')
     else:
-        message = f"`{name}` has left hypixel on {now}"
-    print(message)
-    return message
+        embed = DiscordEmbed(title="Update!", description=f'`{name}` has left `{old_session["gameType"]}`, `{old_session["mode"]}` on {now}')
+        embed.add_embed_field(name='Previous game', value=f"`{old_session['gameType']}`")
+        embed.add_embed_field(name='Previous mode', value=f"`{old_session['mode']}`")
+        embed.add_embed_field(name='Current time', value=now)
+        embed.set_color('0xff0000')
+    
+    return embed
+def write_message(session, name, old_session, old_now): # function for formatting data
+    import datetime as dt
+    now = dt.datetime.now(dt.timezone(dt.timedelta(hours=+1))).strftime('`%B %d %Y`, `%H:%M:%S`')
+    if session['online']:
+        embed = generate_embed(session, name, now, old_session, old_now)
+    else:
+        embed = generate_embed("None", name, now, old_session, old_now)
+    return [embed, now]
 
 def main_loop(_, hypixel_api_key, webhook_link, name):
     import requests
     from time import sleep
     current_session = {"online": False}
     uuid = requests.get("https://api.mojang.com/users/profiles/minecraft/"+name).json()['id']
+    old_now = None
     while True:
         df = requests.get(hypixel_api_key+uuid).json()
         if df['session'] != current_session:
+            old_session = current_session
             current_session = df['session']
-            send_message(webhook_link, write_message(current_session, name))
+            embed, old_now = write_message(current_session, name, old_session, old_now)
+            send_message(webhook_link, embed)
         sleep(1) # prevent ratelimiting
 
-if __name__ == '__main__': # checks if the code is ran as a file
+def main(): # checks if the code is ran as a file
   from kwargs import kwargs
   if kwargs['_']:
     kwargs['hypixel_api_key'] = 'https://api.hypixel.net/status?key='+kwargs['hypixel_api_key']+'&uuid='
@@ -33,3 +64,5 @@ if __name__ == '__main__': # checks if the code is ran as a file
   else:
     print("No external settings found.")
     main_loop(None, 'https://api.hypixel.net/status?key='+input("Enter Hypixel api key:\n")+'&uuid=', input("Enter discord webhook link:\n"), input("Enter player to track:\n"))
+if __name__ == '__main__':
+    main()
